9d3aac04e435e1d05b92811b797aedfa095cbae3
ofer221 <ofer2212@gmail.com>
2022-02-18 23:18:49 +0200

    adding form validators util to work with the useForm hook

---
+++ src/util/formValidators.js
@@ -0,0 +1,25 @@
+export const isURL = str => {
+  const pattern = new RegExp(
+    '^(https?:\\/\\/)?' +
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+      '((\\d{1,3}\\.){3}\\d{1,3}))' +
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+      '(\\?[;&a-z\\d%_.~+=-]*)?' +
+      '(\\#[-a-z\\d_]*)?$',
+    'i',
+  );
+  return !!pattern.test(str);
+};
+export const isEmail = email => {
+  return email.match(
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  );
+};
+
+export const minLength = (val, len) => {
+  return len <= val.length;
+};
+
+export const matchPassword = (pass, conf) => {
+  return pass === conf;
+};
