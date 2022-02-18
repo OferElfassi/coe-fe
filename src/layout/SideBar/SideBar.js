2b95a8fc6c688864ee761c66f91bcde25858ed2b
Dekelbd <dekeldbd@gmail.com>
2022-02-18 23:29:41 +0200

    update sidebar content

--- src/layout/SideBar/SideBar.js
+++ src/layout/SideBar/SideBar.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import Drawer from '@mui/material/Drawer';
+import Box from '@mui/material/Box';
+import PropTypes from 'prop-types';
+import SideBarContent from './SideBarContent';
 
-function SideBar() {
-  return <div>SideBar</div>;
+function SideBar({drawerWidth, drawerState, toggleDrawer}) {
+  return (
+    <Box
+      component="nav"
+      sx={{width: {md: drawerWidth}, flexShrink: {md: 0}}}
+      aria-label="mailbox folders">
+      <Drawer
+        variant="temporary"
+        open={drawerState}
+        onClose={toggleDrawer}
+        ModalProps={{
+          keepMounted: true, // Better open performance on mobile.
+        }}
+        sx={{
+          display: {xs: 'none', sm: 'block', md: 'none'},
+          '& .MuiDrawer-paper': {boxSizing: 'border-box', width: drawerWidth},
+        }}>
+        <SideBarContent />
+      </Drawer>
+      <Drawer
+        variant="permanent"
+        sx={{
+          display: {xs: 'none', sm: 'none', md: 'block'},
+          '& .MuiDrawer-paper': {boxSizing: 'border-box', width: drawerWidth},
+        }}
+        open>
+        <SideBarContent />
+      </Drawer>
+    </Box>
+  );
 }
 
+SideBar.propTypes = {
+  drawerWidth: PropTypes.number.isRequired,
+  drawerState: PropTypes.bool.isRequired,
+  toggleDrawer: PropTypes.func.isRequired,
+};
 export default SideBar;
