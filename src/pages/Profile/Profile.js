ed34559b24d246b5c550de67241a139f499f6d98
Dekelbd <dekeldbd@gmail.com>
2022-02-18 21:55:38 +0200

    testing profile component

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import Container from '@mui/material/Container';
+import useUi from '../../hooks/useUi';
+import useUser from '../../hooks/useUser';
+import ProfileHeader from './ProfileHeader/ProfileHeader';
+import ProfilePosts from './ProfilePosts/ProfilePosts';
 
 function Profile() {
-  return <div>Profile</div>;
+  const {uiActions} = useUi();
+  const {userState} = useUser();
+  useEffect(() => {
+    uiActions.setActivePage('profile');
+  }, []);
+  return (
+    <Container sx={{marginTop: 5}}>
+      <ProfileHeader
+        imageUrl={userState.image.url}
+        fullName={userState.fullName}
+        posts={userState.posts.length}
+        followers={userState.followers.length}
+        following={userState.following.length}
+        about={userState.about}
+      />
+      <ProfilePosts />
+    </Container>
+  );
 }
 
 export default Profile;
