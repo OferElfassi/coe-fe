084068c481071e5c4b37fa8dce61e90fdadfd18a
Dekelbd <dekeldbd@gmail.com>
2022-02-18 23:27:56 +0200

    sidebar header and logo design

---
+++ src/layout/SideBar/SideBarHeader/sideBarHeaderStyle.js
@@ -0,0 +1,13 @@
+const sideBarHeaderStyle = theme => ({
+  logoImg: {
+    height: 54,
+    width: 200,
+  },
+  bulbIcon: {
+    backgroundColor: 'rgba(201,201,201,0.23)',
+    color: '#3d3d3d',
+    marginLeft: 20,
+  },
+});
+
+export default sideBarHeaderStyle;
