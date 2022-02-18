e29ab91718cbd14fec2fa5a18f583d18cd37dfb5
ofer221 <ofer2212@gmail.com>
2022-02-18 23:54:46 +0200

    Updating the useUi Hook

--- src/hooks/useUi.js
+++ src/hooks/useUi.js
@@ -1,5 +1,5 @@
 import {useCallback} from 'react';
-import {useSelector, useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import * as uiActions from '../store/actions/uiActions';
 
 export default function useUi() {
@@ -7,20 +7,29 @@ export default function useUi() {
   const values = useSelector(store => store.ui);
   const dispatch = useDispatch();
 
-  const toggleLoaderIndicator = state => {
-    dispatch(uiActions.toggleLoaderIndicator(state));
+  const toggleLoader = state => {
+    dispatch(uiActions.toggleLoader(state));
   };
 
   const setErrorMessage = msg => {
     dispatch(uiActions.setErrorMessage(msg));
   };
 
+  const setActivePage = pageName => {
+    dispatch(uiActions.setActivePage(pageName));
+  };
+  const toggleNewPostModal = state => {
+    dispatch(uiActions.toggleNewPostModal(state));
+  };
+
   return {
     /** @type {uiState} */
     uiState: values,
     uiActions: {
-      toggleLoaderIndicator: useCallback(toggleLoaderIndicator, []),
+      setActivePage: useCallback(setActivePage, []),
+      toggleLoader: useCallback(toggleLoader, []),
       setErrorMessage: useCallback(setErrorMessage, []),
+      toggleNewPostModal: useCallback(toggleNewPostModal, []),
     },
   };
 }
