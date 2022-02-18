8788a1bf06c2ebd3ea347dfae6ef628ac4ebdee3
ofer221 <ofer2212@gmail.com>
2022-02-18 23:54:19 +0200

    creating the usePost hook to handle post related features

---
+++ src/hooks/usePost.js
@@ -0,0 +1,41 @@
+import {useCallback} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import * as postActions from '../store/actions/postActions';
+
+export default function usePost() {
+  /** @type {postState} */
+  const values = useSelector(store => store.post);
+  const dispatch = useDispatch();
+
+  const getFeedPageData = () => {
+    dispatch(postActions.getFeedPageData());
+  };
+  const addComment = (content, postId) => {
+    dispatch(postActions.addComment(content, postId));
+  };
+  const addReaction = (reactionType, postId) => {
+    dispatch(postActions.addReaction(reactionType, postId));
+  };
+  const removeReaction = (reactionType, reactionId, postId) => {
+    dispatch(postActions.removeReaction(reactionType, reactionId, postId));
+  };
+  const postNewImage = pic => {
+    dispatch(postActions.postNewImage(pic));
+  };
+  const addNewPost = postData => {
+    dispatch(postActions.addNewPost(postData));
+  };
+
+  return {
+    /** @type {postState} */
+    postState: values,
+    postActions: {
+      getFeedPageData: useCallback(getFeedPageData, []),
+      addComment: useCallback(addComment, []),
+      addReaction: useCallback(addReaction, []),
+      removeReaction: useCallback(removeReaction, []),
+      postNewImage: useCallback(postNewImage, []),
+      addNewPost: useCallback(addNewPost, []),
+    },
+  };
+}
